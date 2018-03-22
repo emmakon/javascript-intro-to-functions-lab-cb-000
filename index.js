83aa694f064306c7c34da9cf680f5426d1fe6134
Emma Konrad <emma.konrad92@gmail.com>
2018-03-22 21:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
   var lowercase = "hello";
   if (string.toLowerCase() === lowercase) {
-    returns "I can't hear you!"
+    returns "I can't hear you!";
   }
 }
